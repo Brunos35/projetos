c32eaa5deb845ef438993507a2f274e5b2043006
Brunos35 <87710790+Brunos35@users.noreply.github.com>
2024-02-13 20:12:07 -0300

    ventilador
    Projeto de um ventilador utilizando HTML, CSS e JS

---
+++ ventilador/script.js
@@ -0,0 +1,14 @@
+    function on() {
+        vent.style.animationDuration = 2 + 's'
+    }
+    function off() {
+        vent.style.animationDuration = 0 + 's'
+    }
+
+    function vel1() {
+        vent.style.animationDuration = 1 + 's'
+    }
+
+    function vel2() {
+        vent.style.animationDuration = 0.5 + 's'
+    }
