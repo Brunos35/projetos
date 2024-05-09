20454b55298aab24d1f45741e4ef1dfb37149e71
Bruno <87710790+Brunos35@users.noreply.github.com>
2024-05-09 20:21:18 -0300

    cadastro.php atualizado

--- Cãopanheiro/assets/php/cadastro.php
+++ Cãopanheiro/assets/php/cadastro.php
@@ -5,6 +5,77 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro Cãopanheiro</title>
     <link rel="stylesheet" href="../css/login.css">
+    <style>
+
+        body{
+            background:var(--color1);
+        }
+
+        div.cadastro{
+        width: 100%;
+        height:100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        }
+
+        form{
+            width:50%;
+            background-color:var(--color2);
+            padding-bottom:10px;
+            border-radius:10px;
+        }
+
+        .inputbox{
+            width:95%;
+            margin: 0px 0px 30px 20px;
+        }
+
+        .inputbox input{
+            height: 5vh;
+        }
+
+        div.radio{
+            margin:0 0 20px 20px;
+            color:var(--color1);
+            font-size: 1em;
+            
+        }
+
+        input#env{
+            width: 95%;
+            margin: 5px 0px 15px 20px;
+            height: 5vh;
+            border-radius: 40px;
+            background-color: var(--color1);
+            border: none;
+            outline: none;
+            cursor: pointer;
+            font-size: 1em;
+            font-weight: 600;
+        }
+
+        input#env:hover{
+            background: transparent;
+            color: var(--color3);
+            border: 2px solid var(--color1);
+            transition: all .5s;
+        }
+
+        button#back{
+            color:var(--color3);
+            text-decoration: underline;
+            background: none;
+            box-shadow: none;
+            border: none;
+            font-size: 1em;
+            margin-left: 20px;
+        }
+        button#back > a:visited{
+            color:var(--color1);
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <?php 
  $nome = $_POST['nome'];
@@ -12,45 +83,45 @@
  $senha = $_POST['senha'];
 ?>
 <body>
-    <div class="registro" id="registro">
+    <div class="cadastro" id="cadastro">
         <form action="registro.php" autocomplete="on">
             <h2>Registrar</h2>
             <div class="inputbox">
                 <input type="text" name="nome" id="nome" required value="<?=$nome?>">
-                <label for="nome">Nome</label>
+                <label for="nome">Nome:</label>
             </div>
             <div class="inputbox">
-                <input type="text" name="sobrenome" id="sobrenome" required">
-                <label for="nome">Sobrenome</label>
+                <input type="text" name="sobrenome" id="sobrenome" required>
+                <label for="nome">Sobrenome:</label>
             </div>
 
             <div class="inputbox">
                 <input type="date" name="dataNasc" id="dataNasc">
-                <label for="dataNasc">Data de Nascimento: </label>
+                <label for="dataNasc">Data de Nascimento:</label>
             </div>
 
             <div class="inputbox">
                 <input type="number" name="cpf" id="cpf">
-                <label for="cpf">CPF: </label>
+                <label for="cpf">CPF:</label>
             </div>
         
             <div class="inputbox">
                 <input type="text" name="endereco" id="endereco">
-                <label for="endereco">Endereço</label>
+                <label for="endereco">Endereço:</label>
             </div>
             <div class="inputbox">
                 <input type="tel" name="telefone" id="telefone">
-                <label for="telefone">Número de telefone: </label>
+                <label for="telefone">Número de telefone:</label>
             </div>
             <div class="inputbox">
                 <input type="email" name="email" id="email" required value="<?=$email?>">
-                <label for="email">E-mail</label>
+                <label for="email">E-mail:</label>
             </div>
             <div class="inputbox">
                 <input type="password" name="senha" id="senha" required value="<?=$senha?>">
-                <label for="senha">Senha</label>
+                <label for="senha">Senha:</label>
             </div>
-            <div class="input">
+            <div class="radio">
                 <p>Qual seu objetivo? </p>
                 <input type="radio" name="perfil" id="adotante">
                 <label for="perfil">Adotar</label>
@@ -58,11 +129,10 @@
                 <label for="perfil">Doar</label>
             </div>
 
-            <input type="submit" value="Cadastrar">
+            <input type="submit" value="Cadastrar" id="env">
 
-            <button class="env">Registrar</button>
-        </form>
-    <button class="regs"><a href="Paglogin.php">Sou cadastrado</a></button>
+            <button id="back"><a href="Paglogin.php">Sou cadastrado</a></button>
+        </form>    
     </div>
 </body>
 </html>
